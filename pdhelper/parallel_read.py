b98f8fb62a374962c7725e5d16ac2348feb42cda
HumitoSasakura <chachamusics@outlook.com>
2023-09-28 04:40:18 +0900

    説明追加&成形

--- pdhelper/parallel_read.py
+++ pdhelper/parallel_read.py
@@ -1,7 +1,9 @@
 import pandas as pd
 from multiprocessing import Pool
 
-def _file_read(filename, conf, header, index):
+
+def _file_read(filename: str, conf: str, header: bool, index: bool) -> pd.DataFrame:
+    # ファイル読み込みのための内部関数
     if header:
         header = 0
     else:
@@ -10,7 +12,7 @@ def _file_read(filename, conf, header, index):
         index = 0
     else:
         index = None
-    extention = filename.split(".")[-1].lower()
+    extention = filename.split(".")[-1].lower()  # 拡張子入手
     if extention == "csv":
         if conf == "":
             return pd.read_csv(filename, header=header, index_col=index)
@@ -22,9 +24,21 @@ def _file_read(filename, conf, header, index):
         else:
             return pd.read_excel(filename, sheet_name=conf, header=header, index_col=index)
     else:
+        # 拡張子がcsv,xlsx,xls以外だったらこのエラー
         raise ValueError("FileExtentionError")
 
-def parallel_read(files, conf, header, index):
+
+def parallel_read(files: list[str], conf: list[str], header: list[bool], index: list[bool]):
+    """
+    並列処理でファイルを読み込む関数\n
+    input:\n
+        files  ファイル名のリスト\n
+        conf   追加設定 csvの場合文字コードを(空欄ならutf-8)、excelファイルの場合使うシート名を(空欄なら1枚目のシート)書く\n
+        header ヘッダーついてるかどうか　ついてたらTrue ついてなかったらFalse\n
+        index  インデックスついてるかどうか　ついてたらTrue ついてなかったらFalse\n
+    outout:\n
+        読み込んだDataFrameのリスト
+    """
     args = list(zip(files, conf, header, index))
     with Pool() as pool:
         df_list = pool.starmap(_file_read, args)
