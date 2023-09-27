38ce9543c2a19007ae79434b85d678eb249f19f6
HumitoSasakura <chachamusics@outlook.com>
2023-09-27 16:48:35 +0900

    left_mergeのコードとりあえず(未test)

--- pdhelper/left_merge.py
+++ pdhelper/left_merge.py
@@ -1,4 +1,15 @@
 import pandas as pd
 
-def left_merge():
-    return
+def left_merge(df, df_sub,left_on,right_on,target):
+    sub_len = len(df_sub)
+    df_sub = df_sub[~df_sub[right_on].duplicated()] # 被りを消す
+    if type(target) == list:
+        target.append(right_on)
+    else:
+        target = [target,right_on]
+    if sub_len != len(df_sub):
+        print("mergeするデータに被りがありました。一番上のデータのみを利用しています。")
+    df = pd.merge(df,df_sub.loc[:,target],how="left",left_on=left_on,right_on=right_on)
+    if right_on !=left_on:
+        df.drop(right_on)
+    return df
