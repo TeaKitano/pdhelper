ed40174d74e75e807390c354b2f860f29b78eda0
HumitoSasakura <chachamusics@outlook.com>
2023-10-09 03:45:27 +0900

    pypiに上げる準備

--- pdhelper/__init__.py
+++ pdhelper/__init__.py
@@ -1,2 +1,10 @@
 from .parallel_read import parallel_read
 from .left_merge import left_merge
+
+
+__copyright__    = 'Copyright (C) 2023 Tea Kitano'
+__version__      = '1.0.0'
+__license__      = 'BSD-3-Clause'
+__author__       = 'Tea Kitano'
+__author_email__ = 'chachamusics@outlook.com'
+__url__          = 'https://github.com/TeaKitano/pdhelper'
