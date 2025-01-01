c3d5ba5761cb05963a5b5bb055be99f7afc59cd2
TeaKitano <chachamusics@outlook.com>
2025-01-02 02:28:28 +0900

    バージョン名変更

--- pdhelper/__init__.py
+++ pdhelper/__init__.py
@@ -3,8 +3,8 @@ from .left_merge import left_merge
 
 from pandas import *
 
-__copyright__    = 'Copyright (C) 2023 Tea Kitano'
-__version__      = '1.0.0'
+__copyright__    = 'Copyright (C) 2025 Tea Kitano'
+__version__      = '3.0.0'
 __license__      = 'BSD-3-Clause'
 __author__       = 'Tea Kitano'
 __author_email__ = 'chachamusics@outlook.com'
